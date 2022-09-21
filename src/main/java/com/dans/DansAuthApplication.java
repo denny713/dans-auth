cd236442ce1bd56155314e3d71d074c6420091f8
Denny Afrizal <denny.afrizal713@gmail.com>
2022-09-21 17:10:34 +0700

    Project Skeleton

---
+++ src/main/java/com/dans/DansAuthApplication.java
@@ -0,0 +1,13 @@
+package com.dans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DansAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DansAuthApplication.class, args);
+	}
+
+}
